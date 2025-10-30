60078f627631d9913f1441afb35411c4cd8cc739
ms9849 <hee26368@naver.com>
2025-10-30 18:28:02 +0900

    보스 목둔 사운드 추가

--- Client/Private/Boss_WoodHandState.cpp
+++ Client/Private/Boss_WoodHandState.cpp
@@ -28,6 +28,8 @@ void CBoss_WoodHandState::Start(_bool IsBlend)
 {
 	m_pBoss->Set_AnimIndex("CustomMan_Ninjutsu_LaughingMonk", 1.5f, true);
 	m_pBoss->Get_Transform()->LookAt_XZ(m_pPlayerTransformCom->Get_State(STATE::POSITION));
+
+	m_pGameInstance->PlaySoundOnce(TEXT("Boss_WoodHand.wav"), CHANNELID::EFFECT2, 0.49f);
 }
 
 CBossState* CBoss_WoodHandState::Update(_float fTimeDelta)
